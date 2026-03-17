f5f88d94fd1c1b9482145546da342b3e28b324eb
Zelirety <natuslanus@gmail.com>
2026-03-17 11:42:00 +0100

    push

---
+++ html/data/class_pokemon.js
@@ -0,0 +1,32 @@
+import { Attaque } from "./attaque.js";
+import { Type } from "./type.js";
+
+class Pokemon {
+    static all_pokemons
+    constructor(id, nom, stamina, baseAttaque, baseDefense, types, attaquesRapides, attaquesChargées) {
+        this.id = id;
+        this.nom = nom;
+        this.stamina = stamina;
+        this.baseAttaque = baseAttaque;
+        this.baseDefense = baseDefense;
+        this.types = types;
+        this.attaquesRapides = attaquesRapides;
+        this.attaquesChargées = attaquesChargées;
+    }
+
+    toString(){
+        return this.nom+" : #"+this.id+", ["+this.types+"], [STA: "+this.stamina+
+        ", ATK: "+this.baseAttaque+", DEF: "+this.baseDefense+", Rapides = ["+
+        this.attaquesRapides+"], Chargées = ["+this.attaquesChargées+"]";
+    }
+
+    getTypes(){
+
+        return types;
+    }
+
+    getAttacks(){
+        let attacks = [];
+        for (let i=0; i<this.att)
+    }
+}
