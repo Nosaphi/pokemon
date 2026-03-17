8f4e91403da5f4adacea567e67f6ad5166fbf351
Nosaphi <evan.legarrec56@gmail.com>
2026-03-17 12:28:36 +0100

    toSting type

---
+++ html/data/class_type.js
@@ -0,0 +1,38 @@
+import { Type } from "./type.js";
+
+class Types {
+    static all_types
+    constructor(nom) {
+        this.nom = nom;
+        this.listeType = Type.type_effectiveness[this.nom];
+        fill_types()
+    }
+
+    toString(){
+        efficacite = []
+
+        // Trier les types par efficacité
+        for(const type in this.listeType){
+            const eff = this.listeType[type];
+            if(!efficacite[eff]){
+                efficacite[eff] = [];
+            }
+            efficacite[eff].push(type);
+        }
+
+
+        let res = this.nom + " : ";
+        for(let effi in efficacite){
+            res = res + effi + " = " + efficacite[effi];
+        }
+        return res;
+    }
+
+    fill_types(){
+        Type.type_effectiveness.forEach(type => {
+            if(!all_types[type]){
+                all_types.push(type);
+            }
+        });
+    }
+}
