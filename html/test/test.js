ff7dbe0fbffc2c8a4e13342af8556ba17d4fe634
Zelirety <natuslanus@gmail.com>
2026-03-25 12:07:52 +0100

    getAttacks

--- html/test/test.js
+++ html/test/test.js
@@ -2,23 +2,54 @@ import {Pokemon} from "../data/class_pokemon.js";
 import {Type} from "../data/class_type.js";
 import {Attack} from "../data/class_attack.js";
 
-function getPokemonsByType(typeName){
-    return null; 
-}
+
 console.log("neuille")
-function getPokemonsByAttack(attackName) {
+// function getPokemonsByAttack(attackName) {
+//     Pokemon.fill_all_pokemons();
+//     let listePokemon = []
+//     for (let pokemon of Pokemon.all_pokemons) {
+//         const toutesLesAttaques = pokemon.getAttacks().flat();
+//         for (let attaque of toutesLesAttaques){
+//             console.log(attaque)
+//             if (attaque.includes(attackName)) {
+//                 listePokemon.push(pokemon)
+//                 break
+//             }
+//         }
+//     }
+//     return listePokemon;
+// }
+
+function getPokemonsByType(typeName){
     Pokemon.fill_all_pokemons();
     let listePokemon = []
     for (let pokemon of Pokemon.all_pokemons) {
-        const toutesLesAttaques = pokemon.getAttacks().flat();
-        console.log(toutesLesAttaques)
-        for (let attaque of toutesLesAttaques){
-            if (attaque.includes(attackName)) {
+        const tousLesTypes = pokemon.getTypes().flat();
+        for (let type of tousLesTypes){
+            console.log(type)
+            if (attaque.includes(typeName)) {
                 listePokemon.push(pokemon)
             }
         }
     }
     return listePokemon;
 }
-console.log(getPokemonsByAttack("Tackle"))
+
+function getAttacks(attackName){
+    Pokemon.fill_all_pokemons();
+    let listePokemon = []
+    for (let pokemon of Pokemon.all_pokemons) {
+        const toutesLesAttaques = pokemon.getAttacks().flat();
+        for (let attaque of toutesLesAttaques){
+            // console.log(attaque.id)
+            if (attaque.id === attackName){
+                listePokemon.push(pokemon)
+            }
+        }
+    }
+    return listePokemon
+}
+
+// console.log(getAttacks("Tackle"))
+console.log(getPokemonsByType("water"))
 
